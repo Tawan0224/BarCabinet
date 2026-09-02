03c91380d660b0e404cf9070f0e4341aa7ab9b92
Tawan0224 <soeminminlatt.apple@gmail.com>
2026-09-02 17:55:40 +0700

    Initial Commit

---
+++ BarCabinet/BarCabinetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarCabinetApp.swift
+//  BarCabinet
+//
+//  Created by soe min min latt on 2/9/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct BarCabinetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
